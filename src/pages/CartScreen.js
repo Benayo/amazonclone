da0ce701bd0eefba599bf66ca635a3e540d2e11e
Benayo <ben_odeleye@yahoo.com>
2022-06-04 13:55:00 +0100

    updates

--- src/pages/CartScreen.js
+++ src/pages/CartScreen.js
@@ -1,107 +1,68 @@
 import classes from "./CartScreen.module.css";
 
+import { Link } from "react-router-dom";
 const CartScreen = (props) => {
   const { cartItems, onAddToCart, onRemoveFromCart } = props;
 
   const itemPrice = cartItems.reduce((a, c) => a + c.price * c.qty, 0);
 
-  const taxPrice = itemPrice * 0.14;
-
-  const shippingPice = itemPrice > 2000 ? 0 : 50;
-
-  const totalPrice = itemPrice + taxPrice + shippingPice;
   return (
-    // <div>
-    //   <div className={cartItems.list}>
-    //     <ul className={classes.container}>
-    //       <li>
-    //         <h3>Shopping Cart</h3>
-    //         <div>Price</div>
-    //       </li>
-    //       {cartItems}
-    //     </ul>
-    //   </div>
-    //   {cartItems.length === 0 ? (
-    //     <div>Cart is empty</div>
-    //   ) : (
-    //     cartItems.map((item) => (
-    //       <div key={item.id}>
-    //         <img src={item.image} />
-    //         <div className="cart-name">
-    //           <div>{item.name}</div>
-    //           <div>
-    //             Qty:{" "}
-    //             <select>
-    //               <option value="1">1</option>
-    //               <option value="2">2</option>
-    //               <option value="3">3</option>
-    //             </select>
-    //           </div>
-    //         </div>
-    //         <div>{item.price}</div>
-    //       </div>
-    //     ))
-    //   )}
-    // </div>
     <div className={classes.cart}>
-      <div className={cartItems.list}>
+      <div className={classes.list}>
         <ul className={classes.container}>
           <li>
             <h3>Shopping Cart</h3>
             <div>Price</div>
           </li>
-          {cartItems.totalPrice}
+          <li>
+            {cartItems.length === 0 ? (
+              <div>Cart is empty</div>
+            ) : (
+              cartItems.map((item) => (
+                <div className={classes.frame} key={item.id}>
+                  <div className={classes.images}>
+                    <img
+                      src={item.image}
+                      alt="products"
+                      className={classes.image}
+                    />
+                  </div>
+
+                  <div className={classes.name}>
+                    <Link to={"/products/" + item.id}>{item.name}</Link>
+                    <div>
+                      Qty: {item.qty} items
+                      <div className={classes.buttons}>
+                        <button
+                          onClick={() => onAddToCart(item)}
+                          className={classes.add}
+                        >
+                          +
+                        </button>
+                        <button
+                          onClick={() => onRemoveFromCart(item)}
+                          className={classes.remove}
+                        >
+                          -
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={classes.price}>
+                    {item.qty} x $ {item.price}
+                  </div>
+                </div>
+              ))
+            )}
+          </li>
         </ul>
       </div>
-      <div>{cartItems.length === 0 && <div>Cart is empty</div>}</div>
-      {cartItems.map((item) => (
-        <div key={item.id} className={classes.row}>
-          <img src={item.image} alt="products" />
-          <div className={classes.name}>{item.name}</div>
-          <div className={classes.buttons}>
-            <button onClick={() => onAddToCart(item)} className={classes.add}>
-              +
-            </button>
-            <button
-              onClick={() => onRemoveFromCart(item)}
-              className={classes.remove}
-            >
-              -
-            </button>
-          </div>
-          <div className={classes.text}>
-            {item.qty} x $ {item.price.toFixed(2)}
-          </div>
-        </div>
-      ))}
-
-      {cartItems.length !== 0 && (
-        <div className={classes.actions}>
-          <hr></hr>
-
-          <div className={classes.row}>
-            <div className={classes.price}>Item Price</div>
-            <div className={classes.results}>${itemPrice.toFixed(2)}</div>
-          </div>
-          <div className={classes.row}>
-            <div className={classes.price}>Tax Price</div>
-            <div className={classes.results}>${taxPrice.toFixed(2)}</div>
-          </div>
-          <div className={classes.row}>
-            <strong> Total Price</strong>
-            <div className={classes.results}>${totalPrice.toFixed(2)}</div>
-          </div>
-          <hr />
-          <div>
-            <button
-              className={classes.primary}
-              disabled={cartItems.length === 0}
-            >
-              Proceed to Checkout
-            </button>
-          </div>
-        </div>
-      )}
+      <div className={classes.action}>
+        <h3>Subtotal : ${itemPrice}</h3>
+        <button className={classes.primary} disabled={cartItems.length === 0}>
+          Proceed to Checkout
+        </button>
+      </div>
     </div>
   );
 };
